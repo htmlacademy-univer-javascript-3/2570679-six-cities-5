87a434c0ec103f883ee584a4cfe560c0b3064fe7
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2024-12-09 13:32:13 +0500

    6.2 fix

--- src/components/offer/offer.tsx
+++ src/components/offer/offer.tsx
@@ -106,7 +106,7 @@ function OfferPage({ offersDetails: offers }: OfferPageProps) {
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighbourhood</h2>
             <div className="near-places__list places__list">
-              {<OffersList offers={NearPlacesMock}/>}
+              {<OffersList offers={NearPlacesMock} setActiveOfferCardId={() => {}}/>}
             </div>
           </section>
         </div>
